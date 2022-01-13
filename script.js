1126d2804e6926317c3e392cebceadca7b6fdc15
Ramazan Unal <ramazanfb61@gmail.com>
2022-01-14 02:39:44 +0300

    css train ( commit on "master")

---
+++ script.js
@@ -0,0 +1,18 @@
+var element1 = document.getElementById("first")
+var element2 = document.getElementById("second")
+var element3 = document.getElementById("third")
+
+
+
+function anime(){
+    var element1 = document.getElementById("first")
+    element1.classList.toggle("anime")
+}
+function anime2(){
+    var element2 = document.getElementById("second")
+    element2.classList.toggle("anime")
+}
+function anime3(){
+    var element3 = document.getElementById("third")
+    element3.classList.toggle("anime")
+}
